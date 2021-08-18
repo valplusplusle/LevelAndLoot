dc6700d6acfb4a6cc25d49b5b61a93dea0b47920
Valle <v.kummert@drmnt.de>
2021-08-19 00:10:57 +0200

    make a good top down movement instead of jump and run

--- game.js
+++ game.js
@@ -6,11 +6,12 @@ context.webkitImageSmoothingEnabled = false;
 context.mozImageSmoothingEnabled = false;
 context.imageSmoothingEnabled = false;
 
-context.canvas.width = 256
-context.canvas.height = 224
+context.canvas.width = 800
+context.canvas.height = 600
 
-context.canvas.style.height = context.canvas.height;
 context.canvas.style.width = context.canvas.width;
+context.canvas.style.height = context.canvas.height;
+
 
 playerImage = new Image();
 playerImage.src = "mainChar16x16.png";
@@ -18,7 +19,6 @@ playerImage.src = "mainChar16x16.png";
 player = {
   height: 16,
   width: 16,
-  jumping: true,
   x: 0,
   x_velocity: 0,
   y: 0,
@@ -36,6 +36,9 @@ controller = {
       case 87:
         controller.up = key_state
         break
+      case 83:
+        controller.down = key_state
+        break
       case 68:
         controller.right = key_state
         break
@@ -47,9 +50,11 @@ controller = {
 }
 
 loop = function() {
-  if (controller.up && player.jumping == false) {
-    player.y_velocity -= 20
-    player.jumping = true
+  if (controller.up) {
+    player.y_velocity -= 0.5
+  }
+  if (controller.down) {
+    player.y_velocity += 0.5
   }
   if (controller.left) {
     player.x_velocity -= 0.5
@@ -58,30 +63,39 @@ loop = function() {
     player.x_velocity += 0.5
   }
 
-  player.y_velocity += 1.5 //Gravity
+  // player.y_velocity += 1.5 //Gravity
   player.x += player.x_velocity
   player.y += player.y_velocity
   player.x_velocity *= 0.9
   player.y_velocity *= 0.9 
 
-  if (player.y > 224 - 16 - 0) {
-    player.jumping = false
-    player.y = 224 - 16 - 0
+
+  // bottom wall
+  if (player.y > context.canvas.height - 16) {
+    player.y = context.canvas.height - 16
     player.y_velocity = 0
   }
 
+  // top wall
+  if (player.y < 0) {
+    player.y = 0
+    player.y_velocity = 0
+  }
+
+  // left wall
   if (player.x <= 0) {
     player.x = 0
     player.x_velocity = 0
   }
 
-  if (player.x >= 256 - 16) {
-    player.x = 256 - 16
+  // right wall
+  if (player.x >= context.canvas.width - 16) {
+    player.x = context.canvas.width - 16
     player.x_velocity = 0
   }
 
   context.fillStyle = '#202020'
-  context.fillRect(0, 0, 256, 224)
+  context.fillRect(0, 0, context.canvas.width, context.canvas.height)
   context.drawImage(playerImage, player.x, player.y);
 
 

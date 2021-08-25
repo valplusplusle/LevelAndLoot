e3933fa18dc51a2dd8d9b2cb596406bbcdc2efe2
Valle <v.kummert@drmnt.de>
2021-08-25 23:24:56 +0200

    remove the 80 sprites and just use 1 sheet, make the frist idle and run animation

--- gameEngine/engine.js
+++ gameEngine/engine.js
@@ -14,10 +14,12 @@ canvas.style.width = variableResolution + 'px';
 
 class Player {
   state = 'idle';
+  lastState = 'idle';
   x = 0;
   y = 0;
 
-  idleImage = new Image();
+  sprite = new Image();
+  frame = 0;
   
   constructor() {
     this.height = 48,
@@ -26,8 +28,7 @@ class Player {
     this.y_velocity = 0,
     this.state = 'idle',
     this.jumping = true,
-    this.idleImage.src = "./assets/dd/idle1_1.png"
-
+    this.sprite.src = "./assets/dd/dd_sprite.png"
   }
 
   changeState(newState) {
@@ -39,14 +40,35 @@ class Player {
   }
 
   renderPlayer() {
-    if(this.state === 'idle') {
-      if(this.idleImage.src === "./assets/dd/run_1.png") {
-        context.drawImage(this.idleImage, this.x, this.y);
-        this.idleImage.src = "./assets/dd/idle1_1.png";
-      } else {
-        context.drawImage(this.idleImage, this.x, this.y);
-      }
+    if (this.lastState != this.state) {this.frame = 0; this.lastState = this.state};
+    if (this.frame == 0) {this.frame = Date.now();};
+    const timeNow = Date.now();
 
+    if(this.state === 'idle') {
+      if(timeNow-this.frame <= 200) {
+        context.drawImage(this.sprite, 0, 0, 50, 48, this.x, this.y, 50,48);
+      } else if (timeNow-this.frame > 200){
+        context.drawImage(this.sprite, 50, 0, 50, 48, this.x, this.y, 50,48);
+      }
+      if (timeNow-this.frame >= 600) {this.frame = Date.now();}
+    }
+
+    if(this.state === 'run') {
+      const timeDifference = timeNow-this.frame;
+      if(timeDifference <= 100) {
+        context.drawImage(this.sprite, 0, 144, 50, 48, this.x, this.y, 50,48);
+      } else if (timeDifference > 100 && timeDifference <= 200){
+        context.drawImage(this.sprite, 50, 144, 50, 48, this.x, this.y, 50,48);
+      } else if (timeDifference > 200 && timeDifference <= 300){
+        context.drawImage(this.sprite, 100, 144, 50, 48, this.x, this.y, 50,48);
+      } else if (timeDifference > 300 && timeDifference <= 400){
+        context.drawImage(this.sprite, 150, 144, 50, 48, this.x, this.y, 50,48);
+      } else if (timeDifference > 400 && timeDifference <= 500){
+        context.drawImage(this.sprite, 200, 144, 50, 48, this.x, this.y, 50,48);
+      } else if (timeDifference > 500 && timeDifference <= 600){
+         context.drawImage(this.sprite, 250, 144, 50, 48, this.x, this.y, 50,48);
+      }
+      if (timeNow-this.frame >= 600) {this.frame = Date.now();}
     }
     
   }
@@ -60,7 +82,7 @@ controller = {
   up: false,
   keyListener: function(event) {
     var key_state = event.type == 'keydown' ? true : false
-
+    if (key_state == false) {player.state = 'idle'}
     switch (event.keyCode) {
       case 87:
         controller.up = key_state
@@ -94,16 +116,18 @@ function updateData() {
   }
   if (controller.left) {
     player.x_velocity -= 0.5
+    player.state = 'run';
   }
   if (controller.right) {
     player.x_velocity += 0.5
+    player.state = 'run';
   }
 
-  player.y_velocity += 1.5 //Gravity
+  player.y_velocity += 0.5 //Gravity
   player.x += player.x_velocity
   player.y += player.y_velocity
-  player.x_velocity *= 0.9
-  player.y_velocity *= 0.9 
+  player.x_velocity *= 0.5
+  player.y_velocity *= 0.5 
 
 
   // bottom wall
@@ -112,7 +136,6 @@ function updateData() {
     player.y = context.canvas.height - 48
     player.y_velocity = 0
   }
-
 
   // top wall
   if (player.y < 0) {
@@ -127,8 +150,8 @@ function updateData() {
   }
 
   // right wall
-  if (player.x >= context.canvas.width - 48) {
-    player.x = context.canvas.width - 48
+  if (player.x >= context.canvas.width - 50) {
+    player.x = context.canvas.width - 50
     player.x_velocity = 0
   }
 }

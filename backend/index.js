71c1a0ae1d24de05b3cd8b8b42670cfacd38ca92
Valle <v.kummert@drmnt.de>
2021-09-11 22:26:10 +0200

    add connection handling for opening and closing a session

--- backend/index.js
+++ backend/index.js
@@ -16,23 +16,43 @@ httpServer.listen(3005, () => {
 // create ws server
 var WebSocketServer = require('ws').Server;
 var wss = new WebSocketServer({
-    server: httpServer
+  server: httpServer
 });
 
 // main game connections
 const lobbyArray = [];
 
 wss.on('connection', function connection(ws) {
-    ws.send(JSON.stringify(lobbyArray));
+
   
-    ws.on('message', function incoming(message) {
-        console.log('got a message')
-        let msg = JSON.parse(message);
-        console.log(msg)
-        //ws.send(JSON.stringify(lobbyArray));
-    });
-    
-    ws.on('close', function(){
-      console.log('connection closed handle closing')
-    })
+
+  ws.send(JSON.stringify(lobbyArray));
+
+  ws.on('message', function incoming(message) {
+    let msg = JSON.parse(message);
+    ws.id = msg.id;
+
+    if (lobbyArray.length > 0) {
+      const checkLobbyArray = obj => obj.id === msg.id;
+
+      // check if player object is already in lobby
+      if (lobbyArray.some(checkLobbyArray)) {
+        // if true, update the object
+        objIndex = lobbyArray.findIndex((obj => obj.id === msg.id));
+        lobbyArray[objIndex] = msg;
+      } else {
+        // else add new player
+        lobbyArray.push(msg);
+      }
+    } else {
+      // if no player is online add as first player
+      lobbyArray.push(msg);
+    }
+  });
+
+  ws.on('close', function () {
+    console.log('connection closed handle closing')
+    objIndex = lobbyArray.findIndex((obj => obj.id === ws.id));
+    lobbyArray.splice(objIndex, 1);
+  })
 });
